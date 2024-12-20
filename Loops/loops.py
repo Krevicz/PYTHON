9d424c4fe096bb284d198f491834a10758cdc4fd
Saket Saxena <163630387+Krevicz@users.noreply.github.com>
2024-12-20 21:18:57 +1100

    Add files via upload

---
+++ Loops/loops.py
@@ -0,0 +1,9 @@
+for i in range(0, 14, 2):
+    print(i)
+number = int(input("Enter a number: "))
+for i in range(1, 11, 1):
+    print(f"{number} x {i} = {number * i}")
+
+string = "Hello World"
+for i in string:
+    print(i)
